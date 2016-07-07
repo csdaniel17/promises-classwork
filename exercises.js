61df82e6620adc200b2e9892f21e7409a3e2b2dd
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-07-07 11:40:18 -0400

    more exercises

--- exercises.js
+++ exercises.js
@@ -167,3 +167,39 @@ app.post('/register', function(req, res) {
 app.listen(3000, function() {
   console.log('Listening on port 3000.');
 });
+
+/*
+Rewrite this code is promise-based style.
+Hint: replace the fs module with fs-promise
+
+var fs = require('fs');
+var marked = require('marked');
+fs.readFile('README.md', function(err, buffer) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  var contents = buffer.toString();
+  var html = marked(contents);
+  fs.writeFile('README.html', html, function(err) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log('Wrote README.html');
+  });
+});
+*/
+
+var fs = require('fs-promise');
+var marked = require('marked');
+
+fs.readFile('README.md')
+  .then(function(buffer) {
+    var contents = buffer.toString();
+    var html = marked(contents);
+    return fs.writeFile('README.md', html);
+  })
+  .catch(function(err) {
+    console.error(err.message);
+  });
